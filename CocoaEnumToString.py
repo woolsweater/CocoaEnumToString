06a9842e7f95a5c5fb28fe5ba917fb9df18018d8
Josh Caswell <josh@kraken>
2013-08-14 13:21:08 -0700

    Added argument handling and a sh-bang line. Also changed parsing to use explicit defines of NS_OPTIONS and NS_ENUM instead of importing NSObjCRuntime.h

--- CocoaEnumToString.py
+++ CocoaEnumToString.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python
+
 import itertools
+import argparse
+import sys
+import re
+from os import path
 from clang import cindex
 from clang.cindex import CursorKind
 
-INDENT = "    "
-
 def all_children(node):
-    return itertools.chain(iter([node]), *map(all_children, node.get_children()))
+    return itertools.chain(iter([node]), *map(all_children, 
+                                              node.get_children()))
     
 def all_constant_decls(enum):
     return iter(child for child in all_children(enum) if 
@@ -14,32 +19,95 @@ def all_constant_decls(enum):
 def indent_all_lines(s, indent):
     return '\n'.join(indent + line for line in s.split('\n'))
     
-def format_as_array(enum, prefix=""):
-    all_members = ['[{0.spelling}}]=@"{0.spelling}}"'.format(constant) 
+def format_as_array(enum, prefix, indent):
+    all_members = ['[{0.spelling}]=@"{0.spelling}"'.format(constant) 
                         for constant in all_constant_decls(enum)]
     all_members = ",\n".join(all_members)
                                 
-    return "NSString * const {}StringsFor{}[] = {{{}}};".format(prefix, 
-                                                                enum.spelling, 
-                                                                all_members)
+    return "NSString * const {}StringFor{}[] = {{{}}};".format(prefix,
+                                                               enum.spelling, 
+                                                               all_members)
                                                     
-def format_as_func(enum, prefix):
+def format_as_func(enum, prefix, indent):
     case_str = 'case {0.spelling}:\n{indent}return @"{0.spelling}";'
-    all_cases = [case_str.format(constant, indent=INDENT)
+    all_cases = [case_str.format(constant, indent=indent)
                         for constant in all_constant_decls(enum)]
-    all_cases.append('default:\n{}@"";'.format(INDENT))
+    all_cases.append('default:\n{}@"";'.format(indent))
     all_cases = "\n".join(all_cases)
     
-    switch = "switch( val ){{\n{}\n}}".format(indent_all_lines(all_cases, INDENT))
+    switch = "switch( val ){{\n{}\n}}".format(indent_all_lines(all_cases, 
+                                                               indent))
     
     func_str = "NSString * {}StringFor{name}({name} val){{\n{body}\n}}"
     return func_str.format(prefix, name=enum.spelling, 
-                           body=indent_all_lines(switch, INDENT))
+                           body=indent_all_lines(switch, indent))
+                           
+parser = argparse.ArgumentParser(description="Use libclang to find enums in "
+                                  "the specified Objective-C file and emit a "
+                                  "construct (array or function) to "
+                                  "map between the constant values and "
+                                  "their names.")
+parser.add_argument("--arr", "--array", action="store_const", const="array",
+                    dest="construct", help="Emit an array for the mapping.")
+parser.add_argument("-c", "--construct", default="array",
+                    help="Specify 'function' or any prefix ('f', 'fun', etc.) "
+                         "to emit a function that uses a switch statement for "
+                         "the mapping; specify 'array' or any prefix for "
+                         "an array (this is the default). Whichever of -c, "
+                         "--arr, or --fun occurs last in the argument list "
+                         " will dictate the output.")
+# Allow for either all names to be listed after a single flag, or for 
+# one flag to be used per name
+parser.add_argument("-e", "--enums", action="append", #default=[],
+                    help="Specify particular enums to capture; by default "
+                    "all enums in the given file are used. You may specify "
+                    "any number, but each must be preceded by a flag. Names "
+                    "which are not found in the file are ignored.")
+parser.add_argument("--fun", "--func", "--function", action="store_const", 
+                    const="function", dest="construct", 
+                    help="Emit a function for the mapping.")
+parser.add_argument("-i", "--indent", default="4s",
+                    help="Number and type of character to use for indentation."
+                    " Digit plus either 't' (for tabs) or 's' (spaces), e.g., "
+                    "'4s' (which is the default)")
+#TODO: Add "name" argument, allowing insertion of enum name
+parser.add_argument("-o", "--output",
+                    help="If this argument is present, output should go to a "
+                    "file which will be created at the specified path. An "
+                    "error will be raised if the file already exists.")
+parser.add_argument("-p", "--prefix", default="",
+                    help="Prefix to add to the name of emitted construct, "
+                    "e.g. 'NS'")
+parser.add_argument("file", help="Path to the file which should be parsed.")
+                                           
+
+arguments = parser.parse_args()    
+
+if "function".startswith(arguments.construct):
+    format_enum = format_as_func 
+elif "array".startswith(arguments.construct):
+    format_enum = format_as_array
+else:
+   parser.error("Neither 'function' nor 'array' specified for construct ")
+
+match = re.match(r"(\d*)([st])", arguments.indent)
+if not match.group(2):
+    parser.error("Neither tabs nor spaces specified for indentation.")
+else:
+    indent_char = '\t' if match.group(2) == 't' else ' '
+    indent = indent_char * int(match.group(1) or 1)
     
-    
-target_file_name = "NSFileManager.h"
-prefix = "WSS"
-format_enum = format_as_func
+if arguments.output:
+    if path.exists(arguments.output):
+        sys.stderr.write("Error: Requested output file exists: "
+                         "{}\n".format(arguments.output))
+        sys.exit(1)
+    else:
+        out_f = open(arguments.output, 'w')
+else:
+    out_f = sys.stdout
+  
+target_file_name = arguments.file
 
 # Ignore the fact that system libclang probably doesn't match the version
 # of the Python bindings.
@@ -48,25 +116,27 @@ cindex.Config.set_compatibility_check(False)
 cindex.Config.set_library_file("/Applications/Xcode.app/Contents/Developer/"
                                "Toolchains/XcodeDefault.xctoolchain/usr/lib/"
                                "libclang.dylib")
-                               
-FOUNDATION_HEADERS = ("/Applications/Xcode.app/Contents/Developer/Platforms/"
-                     "MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk/System/"
-                     "Library/Frameworks/Foundation.framework/Versions/C/"
-                     "Headers/")
-# Include NSObjCRuntime.h for the definitions of NS_ENUM and NS_OPTIONS.
-# This causes redefinition errors because
-tu = cindex.TranslationUnit.from_source(FOUNDATION_HEADERS + target_file_name, 
-                                        args=["-ObjC", 
-                                              "-include" + FOUNDATION_HEADERS +
-                                              "NSObjCRuntime.h"])
+
+# Preprocessor macros that resolve into enums; these are defined in
+# NSObjCRuntime.h, but including that directly causes redefinition errors due
+# to it being also imported.
+ns_options_def = ("NS_OPTIONS(_type, _name)=enum _name : "
+                 "_type _name; enum _name : _type")
+ns_enum_def = ("NS_ENUM(_type, _name)=enum _name : _type _name; "
+              "enum _name : _type")
+
+tu = cindex.TranslationUnit.from_source(target_file_name, 
+                                        args=["-ObjC", "-D", ns_enum_def,
+                                              "-D", ns_options_def, "-D",
+                                              "NS_ENUM_AVAILABLE="])
                                               
 enums = [node for node in all_children(tu.cursor) if 
-                        node.kind == CursorKind.ENUM_DECL and
-                        node.location.file.name.find(target_file_name) != -1]
-                        
+                node.kind == CursorKind.ENUM_DECL and
+                node.location.file.name.find(target_file_name) != -1]
+if arguments.enums:
+    enums = filter(lambda enum: enum.spelling in arguments.enums, enums)
+
+               
 for enum in enums:
-    print format_enum(enum, prefix)
-    print '\n'
-                                
-
-
+    out_f.write(format_enum(enum, arguments.prefix, indent))
+    out_f.write("\n\n")
